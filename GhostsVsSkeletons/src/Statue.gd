1d463a8a1458c35896a259c9dc60270f5fd09374
dmglynn <dmglynn@ncsu.edu>
2022-10-22 21:25:00 -0400

    Fixed issues with stairs

--- GhostsVsSkeletons/src/Statue.gd
+++ GhostsVsSkeletons/src/Statue.gd
@@ -11,15 +11,23 @@ func trigger_stairs():
 	var inactive_right_stairs = stairs.get_used_cells_by_id(1)
 	var active_left_stairs = stairs.get_used_cells_by_id(2)
 	var inactive_left_stairs = stairs.get_used_cells_by_id(3)
-
+	var active_platforms = stairs.get_used_cells_by_id(4)
+	var inactive_platforms = stairs.get_used_cells_by_id(5)
+	
 	for stair in active_left_stairs:
 		stairs.set_cellv(stair, 3)
-
+	
 	for stair in inactive_left_stairs:
 		stairs.set_cellv(stair, 2)
-
+	
 	for stair in active_right_stairs:
 		stairs.set_cellv(stair, 1)
-
+	
 	for stair in inactive_right_stairs:
 		stairs.set_cellv(stair, 0)
+	
+	for stair in active_platforms:
+		stairs.set_cellv(stair, 5)
+	
+	for stair in inactive_platforms:
+		stairs.set_cellv(stair, 4)
